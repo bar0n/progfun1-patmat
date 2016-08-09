9a47417409dbb6a7dba90d573fe8b8518fa97454
Baron Dmitry <dbaron@intersog.com>
2016-08-09 18:22:08 +0300

    3

--- patmat/src/main/scala/patmat/Huffman.scala
+++ patmat/src/main/scala/patmat/Huffman.scala
@@ -89,7 +89,7 @@ object Huffman {
     * head of the list should have the smallest weight), where the weight
     * of a leaf is the frequency of the character.
     */
-  def makeOrderedLeafList(freqs: List[(Char, Int)]): List[Leaf] = ???
+  def makeOrderedLeafList(freqs: List[(Char, Int)]): List[Leaf] = freqs.map({ case (ch, w) => Leaf(ch, w) }).sortBy(leaf => leaf.weight)
 
   /**
     * Checks whether the list `trees` contains only one single code tree.
